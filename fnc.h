be3fc83101ba2368e56923b29001eb836eb84dc1
HrvojeAdzic <hrvojeadzic31@gmail.com>
2025-05-28 15:37:10 +0200

    Create fnc.h

---
+++ fnc.h
@@ -0,0 +1,25 @@
+#pragma once
+
+typedef struct {
+	char izvodac[20];
+	char pjesma[20];
+	char album[20];
+}ALBUM;
+
+void izbornik(int);
+void dodajGlazbu();
+void urediGlazbu();
+void ispisGlazbe();
+void pretrazivanjePjesme();
+void pretrazivanjeIzvodaca();
+void pretrazivanjeAlbuma();
+void brisanjeGlazbe();
+void brisanjeDatoteke();
+void izlaz();
+void sortiranje(ALBUM*);
+int comparePjesma(const void* a, const void* b);
+
+extern char odgovor;
+
+
+
