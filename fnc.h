126d0b42170fe07ed4f555f27fbf07ef484d988d
HrvojeAdzic <hrvojeadzic31@gmail.com>
2025-06-06 09:45:54 +0200

    Update fnc.h

--- fnc.h
+++ fnc.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma once
 
 typedef struct {
 	char izvodac[20];
@@ -17,6 +18,7 @@ void delMsc();
 void delDat();
 void izadji();
 void sortiranje(ALBUM*);
+void preimenujDatoteku();
 int compSng(const void* m, const void* n);
 
 extern char odgovor;
